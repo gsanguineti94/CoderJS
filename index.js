941ee6f19b49c990c4a05c60165b315bb1ebe39e
Guido Sanguineti <g.sanguineti@94gmail.com>
2021-12-21 19:04:36 -0300

    Primer commit

---
+++ index.js
@@ -0,0 +1,6 @@
+let nombre = prompt("Ingrese su nombre completo")
+let edad = prompt("Ingrese su edad")
+let color = prompt("Ingrese su color favorito")
+let mensaje = "Tu nombre es" + " " + nombre + ", " + "tienes" + " " + edad + " " + "años de edad y tu color favorito es el" + " " + color
+
+alert(mensaje)
