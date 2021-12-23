c123dbbfe31e09081435b54072bd0f2ec72a0e16
Guido Sanguineti <g.sanguineti94@gmail.com>
2021-12-23 14:04:28 -0300

    Tarea3 final

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
-let numero = prompt("Ingrese un numero")
-if (numero == 1000) {
-    alert("El numero es 1000") 
-}
-if (numero < 1000) {
-    alert("El numero es menor a 1000")
-}
-if (numero > 1000) {
-    alert("El numero es mayor a 1000")
+for (let index = 0; index < 5; index++) {
+    
+    let numero = prompt("Ingrese un numero")
+    if (numero == 1000) {
+        alert("El numero es 1000") 
+    }
+    if (numero < 1000) {
+        alert("El numero es menor a 1000")
+    }
+    if (numero > 1000) {
+        alert("El numero es mayor a 1000")
+    }
 }
 
 alert("Muchas gracias!")
-location.reload()
