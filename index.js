d876b854cf8bb69e569c06f380d3ee9657dd76d6
Guido Sanguineti <g.sanguineti94@gmail.com>
2021-12-24 17:08:30 -0300

    La parte del ingreso anda pero por alguna razon el calculador de totales no, tengo que averiguar por que

--- index.js
+++ index.js
@@ -1,10 +1,30 @@
-let cantidadHog = prompt("Ingrese la cantidad de Hogazas que desea")
-let cantidadMint = prompt("Ingrese la cantidad de Panes de molde integrales que desea")
-let cantidadMbl = prompt("Ingrese la cantidad de Panes de molde blancos que desea")
+var cantidadHog = 0
+var cantidadMint = 0
+var cantidadMbl = 0
+var totalHog = 0
+var totalMint = 0
+var totalMbl = 0
+let precioHog = 300
+let precioMint = 310
+let precioMbl = 240
 
-let totalHog = cantidadHog * 300
-let totalMint = cantidadMint * 310
-let totalMbl = cantidadMbl * 240
+function ingreso() {
+    var cantidadHog = prompt("Cuantas hogazas desea?")
+    var cantidadMint = prompt("Cuantos panes de molde integral desea?")
+    var cantidadMbl = prompt("Cuantas panes de molde blancos desea?")
+}
+
+function totales() {
+    let totalHog = cantidadHog * precioHog
+    let totalMint = cantidadMint * precioMint
+    let totalMbl = cantidadMbl * precioMbls
+}
+
+alert("Bienvenido al cotizador del Negro Holandes!")
+
+ingreso()
+
+totales()
 
 let total = totalHog + totalMint + totalMbl
 
