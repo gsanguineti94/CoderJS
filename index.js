5bda03d6215942fcc41ba5e040d4a62c7a80b420
Guido Sanguineti <g.sanguineti@94gmail.com>
2022-01-11 20:04:48 -0300

    Ya estan los productos falta todo el resto.

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
-let nombre = prompt("Ingrese su nombre completo")
-let edad = prompt("Ingrese su edad")
-let color = prompt("Ingrese su color favorito")
-let mensaje = "Tu nombre es" + " " + nombre + ", " + "tienes" + " " + edad + " " + "años de edad y tu color favorito es el" + " " + color
+class Producto{
+    constructor(nombre, peso, precio, masamadre){
+    this.nombre = nombre;
+    this.peso = peso;
+    this.precio = precio;
+    this.masamadre = masamadre;
+    }
+}
 
-alert(mensaje)
+const Producto1 = new Producto("Pan de molde integral", 950, 340, "No")
+const Producto2 = new Producto("Pan de molde blanco", 950, 265, "No")
+const Producto3 = new Producto("Hogaza", 750, 330, "Si")
+const Producto4 = new Producto("Hogaza integral", 800, 375, "Si")
+const Producto5 = new Producto("Barra rustica", 400, 140, "Si")
+
+let CantidadMint = prompt("Ingrese la cantidad de " + Producto1.nombre + " que desea. " + "Peso por unidad = " + Producto1.peso + "gr. " + "Precio por unidad $" + Producto1.precio + ". Contiene madre? " + Producto1.masamadre)
