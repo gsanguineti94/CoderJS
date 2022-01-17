f81c138c2e8dd2ef959a37684fc144d245d99658
Guido Sanguineti <g.sanguineti@94gmail.com>
2022-01-17 14:57:15 -0300

    Cree el constructor para los productos.

---
+++ arrayProductos.js
@@ -0,0 +1,9 @@
+class Producto{
+    constructor(nombre, precio, masaMadre, cantidadStock){
+    this.nombre = nombre;
+    this.precio = precio;
+    this.masaMadre = masaMadre;
+    this.cantidadStock = cantidadStock;
+    }
+    
+}
