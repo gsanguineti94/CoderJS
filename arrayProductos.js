e5426469fbfc8d17f67918aab0f683f604cd5cf1
Guido Sanguineti <g.sanguineti@94gmail.com>
2022-01-17 22:19:12 -0300

    Modificaciones a index.html: Borre el ul pq no andaba y puse directamente en el body para que corra directamente el script del array. Modificaciones al script: cree el array y agregué un do while para que corra el constructor.

--- arrayProductos.js
+++ arrayProductos.js
@@ -1,9 +1,27 @@
-class Producto{
-    constructor(nombre, precio, masaMadre, cantidadStock){
-    this.nombre = nombre;
-    this.precio = precio;
-    this.masaMadre = masaMadre;
-    this.cantidadStock = cantidadStock;
+class Producto {
+    constructor(nombre, precio, masaMadre, cantidad) {
+        this.nombre = nombre;
+        this.precio = precio;
+        this.masaMadre = masaMadre;
+        this.cantidad = cantidad;
+        this.disponible = true;
     }
-    
 }
+let arrayProductos = [];
+
+do {
+    var comprobacion = prompt("Ingresar el nombre del producto o fin para no agregar mas.").toUpperCase();
+    if (comprobacion === "FIN" || comprobacion === "F") {
+    break;
+    }
+    else {
+        let nombreP = comprobacion;
+        let precioP = prompt("Ingresar precio del producto");
+        var masaMadreP = prompt("El producto contiene masa madre?");
+        let cantidadP = prompt("Cantidad del producto comprada");
+        arrayProductos.push(new Producto(nombreP, precioP, masaMadreP, cantidadP));
+        }
+}
+while (comprobacion != "FIN" || comprobacion != "F");
+
+console.log(arrayProductos)
