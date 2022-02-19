d4aba3d88cff80968dda723b935c3bd8566620ab
Guido Sanguineti <g.sanguineti@94gmail.com>
2022-02-18 21:38:58 -0300

    Cree menu en html y menu.js p/ funcion de click

---
+++ menu.js
@@ -0,0 +1,6 @@
+//vars
+const menuBtn = document.querySelector('.burger');
+const prodsBtn = document.querySelector('.productos-btn');
+const negroBtn = document.querySelector('.negro-btn');
+const nosBtn = document.querySelector('.nosotros-btn');
+const contBtn = document.querySelector('.contacto-btn')
